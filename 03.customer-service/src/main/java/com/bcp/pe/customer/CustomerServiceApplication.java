4bcc07aa15b8bb3108127a37271773890de00e77
matiasdidier <gregorio.rodriguez.p@gmail.com>
2020-07-20 21:32:00 -0500

    config eureka all project

--- 03.customer-service/src/main/java/com/bcp/pe/customer/CustomerServiceApplication.java
+++ 03.customer-service/src/main/java/com/bcp/pe/customer/CustomerServiceApplication.java
@@ -3,8 +3,11 @@ package com.bcp.pe.customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @Slf4j
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
