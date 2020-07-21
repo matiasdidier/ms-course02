6e5667457c6ea31306ff4488f9dfed992a57042f
matiasdidier <gregorio.rodriguez.p@gmail.com>
2020-07-20 20:59:37 -0500

    add registry-service yml

---
+++ 06.registry-service/src/test/java/com/bcp/pe/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp.pe.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
